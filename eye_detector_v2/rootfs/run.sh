b7113b37b907a9370adbc90088ba4da923505894
portfolioasnweb <92236761+portfolioasnweb@users.noreply.github.com>
2026-01-29 21:40:49 -0300

    Update run.sh

--- eye_detector_v2/rootfs/run.sh
+++ eye_detector_v2/rootfs/run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/with-contenv sh
 set -e
 
-echo "[Eye Detector V2] Iniciando o ambiente..."
-
-# O arquivo eye_detector.py foi copiado para a raiz (/) pelo Dockerfile
+echo "[Eye Detector V2] Iniciando serviço de detecção..."
 python3 /eye_detector.py
