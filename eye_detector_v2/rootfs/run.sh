c6b4877a8770c9e9845968714736b6522c6f1e36
portfolioasnweb <92236761+portfolioasnweb@users.noreply.github.com>
2026-01-29 21:55:01 -0300

    Update run.sh

--- eye_detector_v2/rootfs/run.sh
+++ eye_detector_v2/rootfs/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv sh
 set -e
 
-echo "[Eye Detector V2] Iniciando..."
+echo "[Eye Detector V2] Iniciando serviço..."
 
-# O arquivo eye_detector.py deve estar na raiz / devido ao COPY do Dockerfile
+# Executa o python que está na raiz [/]
 exec python3 /eye_detector.py
